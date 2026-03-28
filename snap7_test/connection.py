46e95d57349ea375bd5c9a1e7d39fd4324ee4c79
aihatn <chrastinakamil@gmail.com>
2026-03-28 17:22:51 +0100

    PLC Siemens database reading

--- snap7_test/connection.py
+++ snap7_test/connection.py
@@ -1,27 +1,36 @@
-import time
-import snap7
-import CONST
+import time, snap7, CONST
 from snap7.util import get_int
 
-def main():
+
+def connection():
     client = snap7.client.Client()
 
-    try:
-        print("Připojuji se k PLC...")
-        client.connect(CONST.PLC_IP, CONST.RACK, CONST.SLOT)
+    print("Připojuji se k PLC...")
+    client.connect(CONST.PLC_IP, CONST.RACK, CONST.SLOT)
 
-        if not client.get_connected():
-            print("Nepodařilo se připojit k PLC.")
-            return
-
+    if client.get_connected():
         print("PLC je připojeno!")
-        print("Čtení DB2.DBW0 každou sekundu. Ukončení pomocí CTRL+C.")
+        return client
+    else:
+        raise Exception("Nepodařilo se připojit k PLC.")
 
-        while True:
-            data = client.db_read(CONST.DB_NUMBER, CONST.START_OFFSET, CONST.SIZE)
-            value = get_int(data, 0)
-            print(f"Hodnota INT v DB{CONST.DB_NUMBER}.DBW{CONST.START_OFFSET}: {value}")
-            time.sleep(1)
+
+def database_read(client):
+    print(f"Čtení DB{CONST.DB_NUMBER}.DBW{CONST.START_OFFSET} každou sekundu. Ukončení pomocí CTRL+C.")
+
+    while True:
+        data = client.db_read(CONST.DB_NUMBER, CONST.START_OFFSET, CONST.SIZE)
+        value = get_int(data, 0)
+        print(f"Hodnota INT v DB{CONST.DB_NUMBER}.DBW{CONST.START_OFFSET}: {value}")
+        time.sleep(1)
+
+
+def main():
+    client = None
+
+    try:
+        client = connection()
+        database_read(client)
 
     except KeyboardInterrupt:
         print("\nUkončuji program po stisku CTRL+C...")
@@ -30,7 +39,7 @@ def main():
         print(f"Chyba: {e}")
 
     finally:
-        if client.get_connected():
+        if client and client.get_connected():
             client.disconnect()
         print("Odpojeno od PLC.")
 
